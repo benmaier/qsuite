4f52d48735841749fce2513dbd8a817f608c8ab6
Benjamin Maier <benjaminfrankmaier@gmail.com>
2020-01-31 16:20:54 +0100

    add possibility to add old parameter order

--- qsuite/tools.py
+++ qsuite/tools.py
@@ -5,23 +5,25 @@ A collection of useful tools.
 from qsuite import qconfig
 import numpy as np
 
-def change_result_parameter_order(results, new_order, ignore_measurement_axis=False):
+def change_result_parameter_order(results, new_order, old_order=None, ignore_measurement_axis=False):
     """
     Take the result array and move around the axes such that it corresponds to 
     the order of parameters in `new_order` (a list of the parameter names as
     defined in `external_parameters` and `internal_parameters`).
     """
-    cf = qconfig()
 
     res = results
-
     is_list = type(results) is list
     if is_list:
         res = np.array(res)
 
-    params = [ _[0] for _ in cf.external_parameters + cf.internal_parameters ]
+    if old_order is None:
+        cf = qconfig()
+        params = [ _[0] for _ in cf.external_parameters + cf.internal_parameters ]
+    else:
+        params = old_order
 
-    if ignore_measurement_axis:
+    if ignore_measurement_axis and None in params:
         params.pop(params.index(None))
     
     new_indices = np.array([new_order.index(p) for p in params])
@@ -37,5 +39,5 @@ def change_result_parameter_order(results, new_order, ignore_measurement_axis=Fa
 
 
 
-def change_meanerr_parameter_order(meanerr, new_order):
-    return change_result_parameter_order(meanerr, new_order, True)
+def change_meanerr_parameter_order(meanerr, new_order, old_order=None):
+    return change_result_parameter_order(meanerr, new_order, old_order=old_order, ignore_measurement_axis=True)
